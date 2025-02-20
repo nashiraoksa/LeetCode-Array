df20a27666b7a90a150b8118b37b71ff5e11d77d
Nashira Oksani <103569160+nashiraoksa@users.noreply.github.com>
2025-02-20 10:57:31 +0700

    Time: 0 ms (100.00%) | Memory: 53.6 MB (5.58%) - LeetSync

--- 66-plus-one/plus-one.js
+++ 66-plus-one/plus-one.js
@@ -3,8 +3,18 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    let num = BigInt(digits.join(''));    
-    num = num + BigInt(1);
+    let num;
+
+    if(digits.length < 9){ 
+        num = parseInt(digits.join(''));    
+        num = num + 1;
+    }else {
+        num = BigInt(digits.join(''));    
+        num = num + BigInt(1);
+    }
+
+
+
     let arr = num.toString().split('').map(Number);
     
     return arr;
